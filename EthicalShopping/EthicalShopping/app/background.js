6fb3859ac0aeb0b3cccc2cc3f7b3f4ab9d78e7d2
Sam Dimmer <t-sadim@microsoft.com>
2018-07-24 15:25:01 -0700

    company data is stored on installation, function to get data by company name

--- EthicalShopping/EthicalShopping/app/background.js
+++ EthicalShopping/EthicalShopping/app/background.js
@@ -6,7 +6,8 @@
 
 chrome.runtime.onInstalled.addListener(function () {
   console.log("Ethicly Extension started")
-  // redirects to developer page on install
+  storeCompanyData();
+  //redirects to developer page on install
   var newURL = "https://developer.chrome.com/";
   chrome.tabs.create({ url: newURL });
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
@@ -19,3 +20,31 @@ chrome.runtime.onInstalled.addListener(function () {
     }]);
   });
 });
+
+//get json data from file and store as key value pairs by company name.
+function storeCompanyData(){
+  fetch(chrome.extension.getURL('/company_data.json'))
+        .then((resp) => resp.json())
+        .then(function (jsonData) {
+          var data = jsonData["data"];
+          for (var i = 0; i < data.length; i++)
+          {
+            var obj = data[i]
+            var key = "COMPANY_" + obj["company"];
+            var value = {"rating": obj["rating"], "category": obj["category"],
+            "praise" : obj["praise"], "criticism": obj["criticism"], "information" : obj["information"]};
+            var objToStore = {};
+            objToStore[key] = value;
+            chrome.storage.local.set(objToStore);
+          }
+        })
+}
+
+function getCompanyData(key, callback) {
+  if (key != null) {
+    key = ["COMPANY_" + key];
+    chrome.storage.local.get(key, function(obj) {
+      callback(obj);
+    })
+  }
+}
