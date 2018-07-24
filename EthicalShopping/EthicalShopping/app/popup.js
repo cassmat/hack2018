23ee424db69f4ffacd16bfa93c4d5391dcb17dd4
lmp94@vt.edu <lmp94@vt.edu>
2018-07-24 14:17:17 -0700

    basics of adding from a list to the praises list, bug on the appendChild function

--- EthicalShopping/EthicalShopping/app/popup.js
+++ EthicalShopping/EthicalShopping/app/popup.js
@@ -18,3 +18,32 @@ for (i = 0; i < acc.length; i++) {
     } 
   });
 }
+
+var fakePraiseList = ["test1", "test2", "test3"];
+addPraises(fakePraiseList); 
+
+function addPraises(array) {
+    console.log("in addPraise")
+    var ul = document.getElementById("Praise_Dynamic_Listt");
+    
+    // Loop through list and add to Praise_Dynamic_List 
+    var i = 0;
+    var len = array.length;
+
+    for(; i < len; i++)
+    {
+      // Append 
+      var li = document.createElement("li");
+      li.setAttribute('id', array[i]);
+      li.appendChild(document.createTextNode(array[i]));
+      ul.appendChild(li);
+
+    }
+}
+
+// function removePrase(){
+//     var ul = document.getElementById("dynamic-list");
+//     var candidate = document.getElementById("candidate");
+//     var item = document.getElementById(candidate.value);
+//     ul.removeChild(item);
+// }
