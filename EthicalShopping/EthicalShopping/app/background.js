6e7d6c66b3b6149b7c9e24cb80df01273d7d3af5
lmp94@vt.edu <lmp94@vt.edu>
2018-07-24 11:29:11 -0700

    added logos

--- EthicalShopping/EthicalShopping/app/background.js
+++ EthicalShopping/EthicalShopping/app/background.js
@@ -5,9 +5,8 @@
 'use strict';
 
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log("Extension has started.");
-  });
+  console.log("Extension has started.");
+  //Here we can add what pages we want it to show up on
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [new chrome.declarativeContent.PageStateMatcher({
