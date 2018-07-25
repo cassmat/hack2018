6f737daedb78da462af5518bc8af1044121f6f74
lmp94@vt.edu <lmp94@vt.edu>
2018-07-24 21:52:47 -0700

    phrase

--- EthicalShopping/EthicalShopping/app/popup.js
+++ EthicalShopping/EthicalShopping/app/popup.js
@@ -24,15 +24,16 @@ var fakePraiseList = ["test1", "test2", "test3"];
 var fakeCritisimList = ["test3", "test4", "test5"];
 addPraises(fakePraiseList); 
 addCritisim(fakeCritisimList)
+addCorrectPhrase("This is hardcoded atm");
 
-function addPraises(param) {
+function addPraises(arr) {
     var parentNode = document.getElementById("Praise_Dynamic_List");
 
     // Loop through list and add to Praise_Dynamic_List 
-    for(var i = 0; i < param.length; i++)
+    for(var i = 0; i < arr.length; i++)
     {
       // Append 
-      var str = param[i].toString(); 
+      var str = arr[i].toString(); 
       var node = document.createElement("li");
       var textNode = document.createTextNode(str);
       node.appendChild(textNode);
@@ -40,14 +41,14 @@ function addPraises(param) {
     }
 }
 
-function addCritisim(param) {
+function addCritisim(arr) {
     var parentNode = document.getElementById("Cristisim_Dynamic_List");
 
     // Loop through list and add to Praise_Dynamic_List 
-    for(var i = 0; i < param.length; i++)
+    for(var i = 0; i < arr.length; i++)
     {
       // Append 
-      var str = param[i].toString(); 
+      var str = arr[i].toString(); 
       var node = document.createElement("li");
       var textNode = document.createTextNode(str);
       node.appendChild(textNode);
@@ -55,11 +56,7 @@ function addCritisim(param) {
     }
 }
 
-Cristisim_Dynamic_List
-
-// function removePrase(){
-//     var ul = document.getElementById("dynamic-list");
-//     var candidate = document.getElementById("candidate");
-//     var item = document.getElementById(candidate.value);
-//     ul.removeChild(item);
-// }
+function addCorrectPhrase(str) {
+  var parentNode = document.getElementById("Phrase_Section"); 
+  parentNode.textContent = str
+}
